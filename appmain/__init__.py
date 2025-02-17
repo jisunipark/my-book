19c043fb3fcf14bf78a16d932a3c6948975334fc
Lee <leemi8443@naver.com>
2025-02-17 16:55:06 +0900

    Feat : "__init__.py(appmain)"

--- appmain/__init__.py
+++ appmain/__init__.py
@@ -3,5 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from appmain.routes import main
-
 app.register_blueprint(main)
+
+from appmain.user.routes import user
+app.register_blueprint(user)
