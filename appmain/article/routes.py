f626d5a77aacae9a6c77a40259e99ea171ae2633
YUNA <dbskwh11@naver.com>
2025-02-17 23:04:00 +0900

    Feat: 상품등록 페이지 연결, 사용자 검증, 상품정보 저장 기능 작성

--- appmain/article/routes.py
+++ appmain/article/routes.py
@@ -1,1 +1,83 @@
 # 유나님 / 지선
+#상품 등록 페이지 연결
+#상품 데이터를 보낸 사용자가 인증된 사용자인지 검증
+#상품 정보 저장
+
+from flask import Blueprint, send_from_directory, make_response,jsonify,request
+import sqlite3
+
+from appmain import app
+from appmain.utils import verifyJWT, getJWTContent, savePic
+
+article = Blueprint('article',__name__)
+
+@article.route('/create_article')
+def createArticlePage():
+    return send_from_directory(app.root_path, 'templates/create_article.html')
+
+@article.route('/api/article/create',methods=['POST'])
+def createArticle():
+            
+    headerData = request.headers
+    data = request.form #HTTP 요청의 본문 데이터를 가져온다
+    files = request.files #업로드 파일 객체
+    
+    authToken = headerData.get("authtoken") #사용자 인증
+    
+    payload = {"success":False}
+    
+    if authToken:
+        isValid = getJWTContent(authToken)
+        
+        if isValid:
+            token = getJWTContent(authToken)
+            username = token["username"]
+        
+            category = data.get("category")
+            title = data.get("title")
+            desc = data.get("desc")
+            price = data.get("price")
+            
+            #"picture" 키이름의 첨부 파일
+            if files:
+                #print('createArticle.files', files)
+                picFileName = savePic(files["picture"],username)
+            
+                #업로드 데이터 파일 디버깅 코드
+                #print('createArticle.username',username)
+                #print('createArticle.category',category)
+                #print('createArticle.title',title)
+                #print('createArticle.desc',desc)
+                
+                conn = sqlite3.connect('myBook.db')
+                cursor = conn.cursor()
+                
+                if cursor:
+                    if files:
+                        SQL = 'INSERT INTO articles (author, title, category,description, price, picture) \
+                            VALUES (?,?,?,?,?,?)'
+                        cursor.execute(SQL, (username,title,category,desc,price,picFileName))
+                    else:
+                        SQL = 'INSERT INTO articles (author, title, category, description, price) \
+                            VALUES (?,?,?,?,?)'
+                        cursor.execute(SQL,(username, title, category, desc, price))
+                    rowID = cursor.lastrowid
+                    conn.commit()
+                
+                    #동작 확인용 디버깅 코드
+                    #SQL = 'SELECT * FROM articles'
+                    #cursor.execute(SQL)
+                    #rows = cursor.fetchall()
+                    #for row in rows:
+                    #    print(row)
+                
+                    cursor.close()
+                conn.close()
+            
+                payload = {"success": True, "articleNo":rowID}
+            else:
+                pass
+        else:
+            pass
+    
+    return make_response(jsonify(payload),200)
