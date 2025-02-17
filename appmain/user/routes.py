fe3ea0fb86eb30ab3be9d5fe93730b1343ef03e0
Lee <leemi8443@naver.com>
2025-02-17 17:27:23 +0900

    Refactor: "routes 코드 추가"

--- appmain/user/routes.py
+++ appmain/user/routes.py
@@ -2,6 +2,9 @@
 from flask import Blueprint, send_from_directory, make_response, jsonify, request
 import sqlite3
 import bcrypt     # 비밀번호 암호화
+import secrets
+import jwt
+
 
 from appmain import app
 user = Blueprint('user', __name__)
@@ -41,4 +44,58 @@ def register():
   conn.close()
 
   payload = {"success": True}
+  return make_response(jsonify(payload), 200)
+
+@user.route('/signin')
+def signIn():
+  return send_from_directory(app.root_path, 'templates/signin.html')
+
+@user.route('/api/user/signin', methods=['POST'])
+def getAuth():
+  # 클라이언트가 전달한 데이터를 얻는다.
+  data = request.form
+
+  email = data.get("email")
+  passwd = data.get("passwd")
+
+  # 데이터베이스에 저장되어 있는 비밀번호와 일치하는지 확인한다.
+  conn = sqlite3.connect('myBook.db')
+  cursor = conn.cursor()
+
+  payload = {"authenticated": False, "email": '', "username": '', "authtoken": ''}
+
+  if cursor:
+    SQL = 'SELECT id, username, passwd FROM users WHERE email=?'
+    cursor.execute(SQL, (email,))
+    result = cursor.fetchone()    # 먼저 출력되는 데이터를 가져온다.
+
+    # result[0] : 사용자 고유 Id, result[1] : 사용자 이름, result[2] : 비밀번호
+
+    if result:
+      pwMatch = bcrypt.checkpw(passwd.encode('utf-8'), result[2])
+      id = result[0]
+      username = result[1]
+    else :
+      pwMatch = None
+    
+    # 로그인 토큰을 생성한다.
+    if pwMatch:
+      authkey = secrets.token_hex(16)
+
+      SQL = 'UPDATE users SET authkey=? WHERE id=?'
+      cursor.execute(SQL, (authkey, id))
+      conn.commit()
+
+      # jwt.encode(토큰에 포함할 정보, 서명, 암호화 방법)
+      token = jwt.encode({"id": id, "email": email, "username": username, "authkey": authkey}, app.config["SECRET_KEY"], algorithm='HS256')
+      payload = {"authenticated": True, "email": email, "username": username, "authtoken": token}
+
+      # print('user.signin: %s' % email)
+    else :
+      pass
+
+    cursor.close()
+  conn.close()
+
+  # 응답을 클라이언트에게 전달한다.
   return make_response(jsonify(payload), 200)
