08c097cbae921b7765d035e99a0edf61032a784f
Lisa <jjwwoo28@gmail.com>
2025-02-17 18:17:52 +0900

    feat:reply

--- appmain/reply/routes.py
+++ appmain/reply/routes.py
@@ -1,1 +1,7 @@
-# 혜원님
+from flask import Blueprint, request, make_response, jsonify
+import sqlite3
+from appmain import app
+from appmain.utils import veryfyJWT, getJWTContent
+reply = Blueprint('reply', __name__)
+
+#댓글을 저장
