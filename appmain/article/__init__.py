56e2b1d5c472b02286b415ea19ed5f49688ef6e8
YUNA <dbskwh11@naver.com>
2025-02-17 16:21:06 +0900

    Feat: 상품 정보를 저장하기 위한 데이터베이스 테이블 생성

--- appmain/article/__init__.py
+++ appmain/article/__init__.py
@@ -1,1 +1,24 @@
 # 유나님 / 지선
+#appmain 디렉터리의 웹 서버가 실행될 때마다 함께 실행
+#상품 정보 관리 기능의 초깃값을 설정하는 역할
+
+import sqlite3
+
+comm=sqlite3.connect('myBook.db')
+cursor=conn.cursor()
+
+#SQL 'DROP TABLE article'
+#cursor.execute(SQL)
+
+#상품 정보를 저장하기 위한 데이터베이스 테이블 생성
+SQL = '''
+CREATE TABLE IF NOT EXISTS articles (
+    articleNo INTEGER PRIMARY KEY AUTOINCREMENT,
+    author TEXT NOT NULL,
+    title TEXT NOT NULL,
+    category INTEGER,
+    description TEXT,
+    price INTEGER,
+    picture TEXT
+)
+'''
